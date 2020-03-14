840ab00b8920d7c45cf16a91584113620bba34a7
qrxt <46269438+qrxt@users.noreply.github.com>
2020-03-14 03:52:30 +0300

    добавляет вывод статуса для DropdownRooms

--- src/components/dropdown-rooms/dropdown-rooms.js
+++ src/components/dropdown-rooms/dropdown-rooms.js
@@ -5,31 +5,54 @@ export default class DropdownRooms extends Dropdown {
     this.node
       .accordion(this.options);
 
-    if (this.inputQuantities) {
-      this.inputQuantities.each((_, quantity) => {
-        const quantityLabel = $(quantity)
-          .find(".input-quantity__label");
-        const quantityControls = $(quantity)
-          .find(".input-quantity__wrapper");
-
-        quantityControls.on("click", evt => {
-          console.log(
-            quantityLabel.text(),
-            quantityControls.find("input").val()
+    if (this.controlWrappers) {
+      this.controlWrappers
+        .on("click input", () => {
+          this._setLabelText(
+            this._constructStatusString()
           )
         });
-      })
     }
+  }
+
+  _constructStatusString() {
+    const inputValues = $.map(this.inputs, input => {
+      const inputValue = $(input).val();
+      return Number(inputValue);
+    });
+
+    const labels = $.map(this.inputLabels, label => {
+      return $(label).text();
+    });
+
+    const statusString = labels
+      .reduce((acc, labelText, index) => {
+        return [
+          ...acc,
+          `${ labelText }: ${ inputValues[index] }`
+        ];
+      }, [])
+      .join(", ");
+
+    return statusString;
+  }
+
+  _setLabelText(text) {
+    this.label
+      .text(text);
   }
 
   constructor(node, options) {
     super(node, options);
 
+    this.label = this.node
+      .find(".dropdown__label");
     this.inputQuantities = this.node
       .find(".input-quantity");
 
+    this.controlWrappers = this.node.find(".input-quantity__wrapper");
     this.inputs = this.node.find("input");
-    this.labels = this.node.find(".input-quantity__label");
+    this.inputLabels = this.node.find(".input-quantity__label");
 
     this.init();
   }
