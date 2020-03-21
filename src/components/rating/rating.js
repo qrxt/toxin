0fb4aa4cba4f5a3d73b3df5770ca1e3724b03499
qrxt <46269438+qrxt@users.noreply.github.com>
2020-03-21 03:16:39 +0300

    добавляет star rating

--- src/components/rating/rating.js
+++ src/components/rating/rating.js
@@ -4,16 +4,57 @@ export default class Rating {
     this.labels = this.node
       .find(".rating__item");
     this.inputs = this.node
-      .find("input[type='radio']");
+      .find("input");
+    this._activeClassname = "rating__item--active";
 
     this.init();
   }
 
   init() {
-    $(this.labels).each((_, label) => {
-      $(label).on("mouseover", evt => {
-        console.log("entered");
+    $(this.labels).each((idx, label) => {
+      $(label).hover(() => {
+        this._resetHighlightToIndex(0, 5);
+        this._highlightToIndex(idx);
+      });
+
+      $(label).mouseout(() => {
+        const active = this._findCheckedIndex();
+
+        this._resetHighlightToIndex(0, 5);
+        this._highlightToIndex(active);
+      });
+
+      $(label).click(evt => {
+        this._resetHighlightToIndex(0, 5);
+        this._highlightToIndex(idx);
       });
     });
+  }
+
+  _highlightToIndex(idx) {
+    if (idx >= 0) {
+      $(this.labels)
+        .slice(0, idx + 1)
+        .each((_, label) => {
+          $(label).addClass(this._activeClassname);
+        });
+    }
+  }
+
+  _resetHighlightToIndex(from, to) {
+    $(this.labels)
+      .slice(from, to)
+      .each((_, label) => {
+        $(label)
+          .removeClass(this._activeClassname);
+      });
+  }
+
+  _findCheckedIndex() {
+    return this.inputs
+      .get()
+      .findIndex((input, index) =>
+        $(input).is(":checked")
+      );
   }
 }
